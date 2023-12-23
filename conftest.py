9aaf9f8d2f4d0d3283fb05bf54dcc002eb4cf5d5
natalya-shchepkina <107419433+natalya-shchepkina@users.noreply.github.com>
2023-12-23 17:05:55 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -36,7 +36,7 @@ def browser(request):
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--window-size=1920x1080")
         driver = webdriver.Chrome(options=options)
 
     elif browser_name == "firefox":
